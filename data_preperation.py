0eff3148749a43f947308f03aaa5667247443d79
Tommyphung1 <117569251+Tommyphung1@users.noreply.github.com>
2023-01-05 18:13:49 -0800

    General Setup

---
+++ data_preperation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import statistics as stats
+import matplotlib.pyplot as plt
+import scipy
+
+def format_df_columns(serie, _format):
+    """
+    Input: DateFrame Serie (column)
+    
+    Format the column to a given format,
+    Current working formats int, str
+    
+    Output: DateFrame Serie (column)
+    
+    """
+    
+    None
+    
+def check_and_drop(dataframe):
+    None
