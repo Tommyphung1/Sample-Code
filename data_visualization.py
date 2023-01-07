2f1d546e3a0daa134aa3ecf36bae5d3a49334c9d
Tommyphung1 <117569251+Tommyphung1@users.noreply.github.com>
2023-01-06 22:53:29 -0800

    Added Data visual for dataframe

---
+++ data_visualization.py
@@ -0,0 +1,14 @@
+def dataframe_plot(df, x, y, kind = 'line'):
+    """
+    Inputs: df - DataFrame, x - column name (x-axis), y - column name (y-axis), kind - Type of graph
+
+    Display dataframe graph on a two different column
+    
+    Output: Return the ax for plotting
+    """
+    fig, ax = plt.subplots()
+    ax = df.plot(x, y, kind = kind
+           , title = '{} vs {}'.format(x, y), ax = ax);
+    return ax
+    
+    
