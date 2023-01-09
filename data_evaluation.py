93defb6200993ae3e3897e3cd80095d12b83e284
Tommyphung1 <117569251+Tommyphung1@users.noreply.github.com>
2023-01-08 22:14:37 -0800

    Added Wetch Test

--- data_evaluation.py
+++ data_evaluation.py
@@ -1,7 +1,7 @@
 import scipy.stats as stats
 from math import sqrt
 
-def hypothesis_testing(sample, pop_mean, pop_std , test, alpha, sample_size, two_tailed = False):
+def hypothesis_testing(sample1, sample2 = None, pop_mean, pop_std , test, alpha, sample_size, two_tailed = False):
     # Test the sample mean to see whether the sample mean comes from the population or from a different population
     # H0: There is no difference between population mean and sample mean
     # HA: There is a difference between population mean and sample mean
@@ -9,7 +9,7 @@ def hypothesis_testing(sample, pop_mean, pop_std , test, alpha, sample_size, two
     
     print('Test Conducted: {}-test'.format(test))
     if test == 'z':    #Two sample z test
-        sam_mean = np.mean(sample)
+        sam_mean = np.mean(sample1)
         if two_tailed:
             None
         else:    #One sample z test
@@ -30,8 +30,8 @@ def hypothesis_testing(sample, pop_mean, pop_std , test, alpha, sample_size, two
     # Use for small sample size or population std is not known
     # Test to see if means are different
     # Can be solved with p_value or t_stat
-        sam_mean = np.mean(sample)
-        sample_size = len(sample)
+        sam_mean = np.mean(sample1)
+        sample_size = len(sample1)
         num = sam_mean - pop_mean
         dem = np.std(sample, ddof= 1)/np.sqrt(sample_size)
         t = num/dem
@@ -42,6 +42,43 @@ def hypothesis_testing(sample, pop_mean, pop_std , test, alpha, sample_size, two
             p_val = results[1]/2
         else:
             p_val = results[1]
-    print('Results: Failed to reject Null Hypothesis' if p_val >= alpha else 'Results: Reject Null Hypothesis')
-    print('p_val: {}|alpha: {} t_stat: {}|t_crit: {}'.format(round(p_val,4),alpha, round(t, 4), round(t_crit, 4)))
-    return p_val
+        print('Results: Failed to reject Null Hypothesis' if p_val >= alpha else 'Results: Reject Null Hypothesis')
+        print('p_val: {}|alpha: {} t_stat: {}|t_crit: {}'.format(round(p_val,4),alpha, round(t, 4), round(t_crit, 4)))
+        return results
+    
+    elif test == 't2s':   
+    # Two sample t test
+    # Only one function is needed for this test that only need the two independent samples to calculate
+        results = stats.ttest_ind(experimental, control)
+        
+        print('Results: Failed to reject Null Hypothesis' if results[1] >= alpha else 'Results: Reject Null Hypothesis')
+        print('p_val: {}|alpha: {}'.format(round(results[1],4),alpha))
+        return results
+
+    elif test == 'w':
+    # Welch Test
+    # When the variances or sample size arent the same.
+    # If variance are the same, the t-test are good enough
+        var1 = sample1.var(ddof= 1)
+        var2 = sample2.var(ddof= 1)
+
+        diff_mean = np.mean(sample1) - np.mean(sample2)
+        pooled_var = var1/len(sample1) + var2/len(sample2)
+
+        num = (var1 / len(sample1) + var2 / len(sample2)) **2
+        dem = var1**2/((len(sample1)**2)*(len(sample1)-1)) + var2**2/((len(sample2)**2)*(len(sample2)-1))
+
+        t = np.abs(diff_mean / np.sqrt(pooled_var))    # t statistic
+        df = num/dem    # Degree of freedom
+
+        if two_tailed:
+            result = (1 - stats.t.cdf(t,df))*2    
+        else: 
+            result = (1 - stats.t.cdf(t,df))
+
+        print('Results: Failed to reject Null Hypothesis' if result >= alpha else 'Results: Reject Null Hypothesis')
+        print('p_val: {}|alpha: {}'.format(round(result,4),alpha))
+        return result
+    
+    
+    
